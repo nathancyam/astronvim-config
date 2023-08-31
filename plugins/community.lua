a9ce75605ffa591a1d07afe0babfa24b89bf9047
Nathan Yam <nathancyam@gmail.com>
2023-08-31 23:41:34 +1000

    add gh copilot with kanagawa

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  {
+    "github/copilot.vim",
+    lazy = false,
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
