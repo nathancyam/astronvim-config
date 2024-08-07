a99755b06c4a85d70636a1e7b6d118e5e5926c80
nathanyam <nathan.yam@playhq.com>
2024-08-07 21:44:04 +1000

    chore: Install vim-test and db

--- lua/community.lua
+++ lua/community.lua
@@ -33,6 +33,22 @@ return {
     end,
     lazy = false,
   },
+  {"vim-test/vim-test"},
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup(--[[optional config]])
+    end,
+  },
   {
     "easymotion/vim-easymotion",
     lazy = false,
